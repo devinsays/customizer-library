e2f222641786e4cb891cec34e35cf0fb75fd9498
Devin Price <devin@wptheming.com>
2014-08-15 17:55:31 -0500

    Public release.

---
+++ customizer-library.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Customizer Library
+ *
+ * @package		Customizer_Library
+ * @author		Devin Price, The Theme Foundary
+ * @license   	GPL-2.0+
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// Exit if the Customizer_Library is already in use.
+if ( ! class_exists( 'Customizer_Library' ) ) {
+	return;
+}
+
+// Helper functions to output the customizer controls.
+require plugin_dir_path( __FILE__ ) . 'extensions/interface.php';
+
+// Helper functions for customizer sanitization.
+require plugin_dir_path( __FILE__ ) . 'extensions/sanitization.php';
+
+// Helper functions to build the inline CSS.
+require plugin_dir_path( __FILE__ ) . 'extensions/style-builder.php';
+
+// Helper functions for fonts.
+require plugin_dir_path( __FILE__ ) . 'extensions/fonts.php';
+
+// Utility functions for the customizer.
+require plugin_dir_path( __FILE__ ) . 'extensions/utilities.php';
+
+// Customizer preview functions.
+require plugin_dir_path( __FILE__ ) . 'extensions/preview.php';
+
+// Custom controls for the theme customizer.
+require plugin_dir_path( __FILE__ ) . 'custom-controls/textarea.php';
+
+/**
+ * Class wrapper with useful methods for interacting with the theme customizer.
+ */
+class Customizer_Library {
+
+	/**
+	 * The one instance of Customizer_Library.
+	 *
+	 * @since 1.0.0.
+	 *
+	 * @var   Customizer_Library_Styles    The one instance for the singleton.
+	 */
+	private static $instance;
+
+	/**
+	 * The array for storing $options.
+	 *
+	 * @since 1.0.0.
+	 *
+	 * @var   array    Holds the options array.
+	 */
+
+	public $options = array();
+
+	/**
+	 * Instantiate or return the one Customizer_Library instance.
+	 *
+	 * @since  1.0.0.
+	 *
+	 * @return Customizer_Library
+	 */
+	public static function instance() {
+		if ( is_null( self::$instance ) ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	public function add_options( $options = array() ) {
+		$this->options = array_merge( $options, $this->options );
+	}
+
+	public function get_options() {
+		return $this->options;
+	}
+
+}
---
+++ extensions/interface.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * Builds out customizer options
+ *
+ * @package 	Customizer_Library
+ * @author		Devin Price
+ */
+
+if ( ! function_exists( 'customizer_library_register' ) ) :
+/**
+ * Configure settings and controls for the theme customizer
+ *
+ * @since  1.0.0.
+ *
+ * @param  object $wp_customize The global customizer object.
+ * @return void
+ */
+function customizer_library_register( $wp_customize ) {
+
+	$customizer_library = Customizer_Library::Instance();
+	$options = $customizer_library->get_options();
+
+	$sections = $options['sections'];
+
+	if ( isset( $sections ) ) {
+		foreach( $sections as $section ) {
+			$wp_customize->add_section( $section['id'], array(
+				'title' => $section['title'],
+				'priority' => $section['priority'],
+			) );
+		}
+	}
+
+	if ( $options ) {
+
+		foreach( $options as $option ) {
+
+			if ( isset( $option['type'] ) ) {
+
+				if ( isset( $option['default'] ) ) {
+					$default = array( 'default' => $option['default'] );
+				}
+
+			    $wp_customize->add_setting( $option['id'], $default );
+
+				switch ( $option['type'] ) {
+
+					case 'select':
+
+						if ( !isset( $option['sanitize_callback'] ) ) {
+							$option['sanitize_callback'] = 'customizer_library_sanitize_choices';
+						}
+
+						$wp_customize->add_control( $option['id'], array(
+							'type'    => $option['type'],
+					        'label'   => $option['label'],
+					        'section' => $option['section'],
+					        'choices' => $option['choices'],
+					        'sanitize_callback' => $option['sanitize_callback']
+					    ) );
+
+					    break;
+
+					case 'checkbox':
+
+						if ( !isset( $option['sanitize_callback'] ) ) {
+							$option['sanitize_callback'] = 'customizer_library_sanitize_checkbox';
+						}
+
+						$wp_customize->add_control( $option['id'], array(
+							'type'    => $option['type'],
+					        'label'   => $option['label'],
+					        'section' => $option['section'],
+					        'sanitize_callback' => $option['sanitize_callback']
+					    ) );
+
+					    break;
+
+				    case 'color':
+
+						if ( !isset( $option['sanitize_callback'] ) ) {
+							$option['sanitize_callback'] = 'sanitize_hex_color';
+						}
+
+				    	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize,
+				    		$option['id'], array(
+								'label'   => $option['label'],
+								'section' => $option['section'],
+								'sanitize_callback' => $option['sanitize_callback']
+						) ) );
+
+						break;
+
+					case 'upload':
+
+						if ( !isset( $option['sanitize_callback'] ) ) {
+							$option['sanitize_callback'] = 'customizer_library_sanitize_file_url';
+						}
+
+				    	$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize,
+				    		$option['id'], array(
+								'label'   => $option['label'],
+								'section' => $option['section'],
+								'sanitize_callback' => $option['sanitize_callback']
+						) ) );
+
+						break;
+
+					case 'textarea':
+
+						if ( !isset( $option['sanitize_callback'] ) ) {
+							$option['sanitize_callback'] = 'customizer_library_sanitize_text';
+						}
+
+				    	$wp_customize->add_control( new Customizer_Library_Textarea( $wp_customize,
+				    		$option['id'], array(
+								'label'   => $option['label'],
+								'section' => $option['section'],
+								'sanitize_callback' => $option['sanitize_callback']
+						) ) );
+
+						break;
+
+			    }
+
+			}
+		}
+	}
+}
+endif;
+
+add_action( 'customize_register', 'customizer_library_register', 100 );
